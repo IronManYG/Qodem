15aa720fdafa134ed0237608147b8b03a1e8ed92
hussain.yg <hussain.yg2010@gmail.com>
2023-08-16 23:42:41 +0300

    Add Badges.kt.

---
+++ profile/profile-domain/src/main/java/com/ironmanyg/profile_domain/user/Badges.kt
@@ -0,0 +1,70 @@
+package com.ironmanyg.profile_domain.user
+
+/**
+ * Enum class representing different badges that can be awarded to blood donors based on their contributions.
+ */
+enum class Badge {
+    /** Awarded to users who donate blood for the first time. */
+    FirstTimeDonor,
+
+    /** Awarded to users who donate blood on a regular basis within a specific timeframe. */
+    FrequentDonor,
+
+    /** Awarded to users whose blood donation directly contributed to saving a life. */
+    LifeSaver,
+
+    /** Awarded to users who have made a significant impact on their local community by donating blood. */
+    CommunityHero,
+
+    /** Awarded to users with rare blood types who regularly donate to help others with similar blood types. */
+    BloodGroupChampion,
+
+    /** Awarded to users who donate double red blood cells, helping patients in need of specific components. */
+    DoubleRedDonor,
+
+    /** Awarded to users who donate platelets, which are crucial for patients with clotting disorders. */
+    PlateletDonor,
+
+    /** Awarded to users with a high number of blood donations, signifying their dedication to the cause. */
+    Hematologist,
+
+    /** Awarded to users who donate blood during emergency blood drives or critical shortage situations. */
+    EmergencyResponder,
+
+    /** Awarded to users who maintain a consistent streak of blood donations over a period of time. */
+    DonationStreak,
+
+    /** Awarded to users who have collectively donated a gallon (or other significant volume) of blood. */
+    GoldenGallon,
+
+    /** Awarded to users who participate in donation drives for charitable causes or disaster relief efforts. */
+    BleedForGood,
+
+    /** Awarded to users who actively promote blood donation through social media or community events. */
+    DonorAdvocate,
+
+    /** Awarded to users who organize and host successful blood drives in their communities. */
+    BloodDriveHost,
+
+    /** Awarded to users who participate in group donation efforts, encouraging friends, family, and colleagues to donate. */
+    TeamPlayer,
+
+    /** Awarded to users who have made a substantial lifetime commitment to blood donation. */
+    LifetimeDonorAchievement,
+
+    /** Awarded to users who help educate others about the importance and benefits of blood donation. */
+    DonationEducator,
+
+    /** Awarded to users who participate in virtual blood donation events or campaigns. */
+    VirtualDonor,
+
+    /** Awarded to users who engage in public speaking or media appearances to promote blood donation. */
+    DonationAmbassador,
+
+    /** Awarded to users who donate blood internationally or participate in cross-border blood donation efforts. */
+    GlobalGiver
+}
+
+
+
+
