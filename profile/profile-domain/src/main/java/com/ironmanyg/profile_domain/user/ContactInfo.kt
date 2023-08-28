e624ed1778f27586670532833dffa03ee3a8a434
hussain.yg <hussain.yg2010@gmail.com>
2023-08-28 19:28:07 +0300

    Add ContactInfo.kt.

---
+++ profile/profile-domain/src/main/java/com/ironmanyg/profile_domain/user/ContactInfo.kt
@@ -0,0 +1,15 @@
+package com.ironmanyg.profile_domain.user
+
+/** Represents a user's contact information.
+ *
+ * @property address The detailed address of the user.
+ * @property phoneNumber The phone number of the user.
+ * @property email The email address of the user.
+ * @property emergencyContact The emergency contact details for the user. Can be null if not provided.
+ */
+data class ContactInfo(
+    val address: Address,
+    val phoneNumber: String,
+    val email: String,
+    val emergencyContact: String?
+)
