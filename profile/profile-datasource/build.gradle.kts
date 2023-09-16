6d7a298cc227441b4a312c0cf1464fb4115cb20a
hussain.yg <hussain.yg2010@gmail.com>
2023-09-16 18:38:18 +0300

    Add EndPoints.kt.

--- profile/profile-datasource/build.gradle.kts
+++ profile/profile-datasource/build.gradle.kts
@@ -2,5 +2,15 @@ apply(
     from = "$rootDir/library-build.gradle"
 )
 
+plugins {
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
+}
+
 dependencies {
+    "implementation"(project(Modules.profile_domain))
+
+    "implementation"(Ktor.core)
+    "implementation"(Ktor.contentNegotiation)
+    "implementation"(Ktor.kotlinxSerialization)
+    "implementation"(Ktor.android)
 }
---
+++ profile/profile-datasource/src/main/java/com/ironmanyg/profile_datasource/network/EndPoints.kt
@@ -0,0 +1,33 @@
+package com.ironmanyg.profile_datasource.network
+
+/**
+ * An object holding the constants for the API endpoints used in the app.
+ * Keeping the endpoint URLs in a single place allows for easier management and updates.
+ */
+object EndPoints {
+
+    /**
+     * The base URL for the API. All other endpoints will be relative to this URL.
+     */
+    private const val BASE_URL = "https://api.jsonbin.io/v3/b/"
+
+    /**
+     * Enumeration grouping all available API endpoints, providing better type safety and logical grouping.
+     */
+    enum class Endpoint(val path: String) {
+        /**
+         * Endpoint to fetch the profile information from the API.
+         */
+        PROFILE("profile"),
+    }
+
+    /**
+     * Utility method to construct a full URL for a given endpoint.
+     *
+     * @param endpoint The [Endpoint] to construct the URL for.
+     * @return The full URL as a [String].
+     */
+    fun getFullUrl(endpoint: Endpoint): String {
+        return "$BASE_URL${endpoint.path}"
+    }
+}
