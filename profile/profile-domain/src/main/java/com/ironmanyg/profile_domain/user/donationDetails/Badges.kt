0f5d95b2a93adc2be0e8b8affaea0db20e71b080
hussain.yg <hussain.yg2010@gmail.com>
2023-09-17 00:50:41 +0300

    Update Badges.kt.

--- profile/profile-domain/src/main/java/com/ironmanyg/profile_domain/user/donationDetails/Badges.kt
+++ profile/profile-domain/src/main/java/com/ironmanyg/profile_domain/user/donationDetails/Badges.kt
@@ -5,105 +5,81 @@ package com.ironmanyg.profile_domain.user.donationDetails
  * specific contributions and achievements in the blood donation community. Each enum constant represents
  * a specific badge that a donor can receive.
  */
-enum class Badge {
+enum class Badge(val humanReadableName : String) {
     /** Awarded to users who donate blood for the first time. */
-    FirstTimeDonor,
+    FirstTimeDonor("First Time Donor"),
 
     /** Awarded to users who donate blood on a regular basis within a specific timeframe. */
-    FrequentDonor,
+    FrequentDonor("Frequent Donor"),
 
     /** Awarded to users whose blood donation directly contributed to saving a life. */
-    LifeSaver,
+    LifeSaver("Life Saver"),
 
     /** Awarded to users who have made a significant impact on their local community by donating blood. */
-    CommunityHero,
+    CommunityHero("Community Hero"),
 
     /** Awarded to users with rare blood types who regularly donate to help others with similar blood types. */
-    BloodGroupChampion,
+    BloodGroupChampion("Blood Group Champion"),
 
     /** Awarded to users who donate double red blood cells, helping patients in need of specific components. */
-    DoubleRedDonor,
+    DoubleRedDonor("Double Red Donor"),
 
     /** Awarded to users who donate platelets, which are crucial for patients with clotting disorders. */
-    PlateletDonor,
+    PlateletDonor("Platelet Donor"),
 
     /** Awarded to users with a high number of blood donations, signifying their dedication to the cause. */
-    Hematologist,
+    Hematologist("Hematologist"),
 
     /** Awarded to users who donate blood during emergency blood drives or critical shortage situations. */
-    EmergencyResponder,
+    EmergencyResponder("Emergency Responder"),
 
     /** Awarded to users who maintain a consistent streak of blood donations over a period of time. */
-    DonationStreak,
+    DonationStreak("Donation Streak"),
 
     /** Awarded to users who have collectively donated a gallon (or other significant volume) of blood. */
-    GoldenGallon,
+    GoldenGallon("Golden Gallon"),
 
     /** Awarded to users who participate in donation drives for charitable causes or disaster relief efforts. */
-    BleedForGood,
+    BleedForGood("Bleed for Good"),
 
     /** Awarded to users who actively promote blood donation through social media or community events. */
-    DonorAdvocate,
+    DonorAdvocate("Donor Advocate"),
 
     /** Awarded to users who organize and host successful blood drives in their communities. */
-    BloodDriveHost,
+    BloodDriveHost("Blood Drive Host"),
 
     /** Awarded to users who participate in group donation efforts, encouraging friends, family, and colleagues to donate. */
-    TeamPlayer,
+    TeamPlayer("Team Player"),
 
     /** Awarded to users who have made a substantial lifetime commitment to blood donation. */
-    LifetimeDonorAchievement,
+    LifetimeDonorAchievement("Lifetime Donor Achievement"),
 
     /** Awarded to users who help educate others about the importance and benefits of blood donation. */
-    DonationEducator,
+    DonationEducator("Donation Educator"),
 
     /** Awarded to users who participate in virtual blood donation events or campaigns. */
-    VirtualDonor,
+    VirtualDonor("Virtual Donor"),
 
     /** Awarded to users who engage in public speaking or media appearances to promote blood donation. */
-    DonationAmbassador,
+    DonationAmbassador("Donation Ambassador"),
 
     /** Awarded to users who donate blood internationally or participate in cross-border blood donation efforts. */
-    GlobalGiver,
+    GlobalGiver("Global Giver"),
 
     /**
      * Unknown badge. This is the default value for the enum constant and should not be used.
      */
-    UNKNOWN;
+    UNKNOWN("Unknown");
 
     companion object {
-
         /**
-         * A utility function to get a Badge enum entry from its name.
-         * The name parameter is the String representation of the badge which is to be converted to a Badge enum type.
+         * Returns the Badge corresponding to the given human-readable name, ignoring case and extra spaces.
          *
-         * @param name the name of the badge in a human-readable format.
-         * @return the corresponding Badge enum entry, or UNKNOWN if the name does not match any known badges.
+         * @param name The human-readable name of the badge.
+         * @return The corresponding Badge, or UNKNOWN if no match is found.
          */
-        fun fromName(name: String) :Badge {
-            return when(name) {
-                "First time donor" -> FirstTimeDonor
-                "Frequent donor" -> FrequentDonor
-                "Life saver" -> LifeSaver
-                "Community hero" -> CommunityHero
-                "Blood group champion" -> BloodGroupChampion
-                "Double redDonor" -> DoubleRedDonor
-                "Platelet donor" -> PlateletDonor
-                "Hematologist" -> Hematologist
-                "Emergency responder" -> EmergencyResponder
-                "Donation streak" -> DonationStreak
-                "Golden gallon" -> GoldenGallon
-                "Bleed for good" -> BleedForGood
-                "Donor advocate" -> DonorAdvocate
-                "Blood drive host" -> BloodDriveHost
-                "Team player" -> TeamPlayer
-                "Lifetime donor achievement" -> LifetimeDonorAchievement
-                "Donation educator" -> DonationEducator
-                "Virtual donor" -> VirtualDonor
-                "Donation ambassador" -> DonationAmbassador
-                "Global giver" -> GlobalGiver
-                else -> UNKNOWN
-            }
+        fun fromName(name: String): Badge {
+            return values().find { it.humanReadableName.equals(name.trim(), true) } ?: UNKNOWN
         }
     }
 }
